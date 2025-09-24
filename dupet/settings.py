7600739da8c8649f084a9306a8e55a4564c33159
Caleb Falcão <calebfalcao1@gmail.com>
2025-09-23 22:39:03 -0300

    update settings.py

--- dupet/settings.py
+++ dupet/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'chave-insegura-para-dev')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost").split(",")
 
 
 # Application definition
